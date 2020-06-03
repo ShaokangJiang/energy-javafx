e8ab08148c2f6b3dea5a7f8a1541dd837106036b
ShaokangJiang <32856266+ShaokangJiang@users.noreply.github.com>
2020-06-03 11:33:22 -0500

    A more front handler

--- application/knapsack.java
+++ application/knapsack.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 
+/**
+ * NOT A REAL knapsack Modified version with memorize feature
+ * 
+ * @author Shaokang Jiang
+ *
+ */
 public class knapsack {
 
 	static HashMap<String, Double> use;
+	private static DecimalFormat df = new DecimalFormat("#.000");
 
 	// A utility function that returns
 	// maximum of two integers
@@ -13,8 +21,8 @@ public class knapsack {
 	}
 
 	static double knapSackHelper(double W, int wt[], int val[], int n) {
-		if (use.get("" + n + "," + W) != null) {
-			return use.get("" + n + "," + W);
+		if (use.get("" + n + "," + df.format(W)) != null) {
+			return use.get("" + n + "," + df.format(W));
 		}
 		return knapSack(W, wt, val, n);
 	}
@@ -25,7 +33,7 @@ public class knapsack {
 	static double knapSack(double W, int wt[], int val[], int n) {
 		// Base Case
 		if (n == 0 || W == 0) {
-			use.put("" + n + "," + W, 0.00);
+			use.put("" + n + "," + df.format(W), 0.00);
 			return 0;
 		}
 
@@ -35,7 +43,7 @@ public class knapsack {
 		// in the optimal solution
 		if (wt[n - 1] > W) {
 			double value = knapSackHelper(W, wt, val, n - 1);
-			use.put("" + n + "," + W, value);
+			use.put("" + n + "," + df.format(W), value);
 			return value;
 		}
 		// Return the maximum of two cases:
@@ -45,7 +53,7 @@ public class knapsack {
 			double case1 = knapSackHelper(W - wt[n - 1], wt, val, n - 1);
 			double case2 = knapSackHelper(W, wt, val, n - 1);
 			double end = max(val[n - 1] + case1, case2);
-			use.put("" + n + "," + W, end);
+			use.put("" + n + "," + df.format(W), end);
 			return end;
 		}
 	}
@@ -64,7 +72,7 @@ public class knapsack {
 			// [w-wt[i-1]]) as in Knapsack table. If
 			// it comes from the latter one/ it means
 			// the item is included.
-			if (res == use.get("" + (i - 1) + "," + w)) {
+			if (res == use.get("" + (i - 1) + "," + df.format(w))) {
 				toRe[i - 1] = false;
 				continue;
 			} else {
@@ -80,9 +88,17 @@ public class knapsack {
 			}
 		}
 		return toRe;
-
 	}
 
+	public static boolean[] handler(double W, int wt[], int val[]) {
+		use = new HashMap<String, Double>();
+		int n=val.length;
+		double res = knapSack(W, wt, val, n);
+		System.out.print("Optimal Solution:" + res);
+		return getLastSelection(n, res, wt, val, W);
+	}
+
+	
 	// Driver program to test
 	// above function
 	public static void main(String args[]) {
@@ -90,15 +106,16 @@ public class knapsack {
 		 * int val[] = new int[] { 92,57,49,68,60,43,67,84,87,72 }; int wt[] = new int[]
 		 * { 23,31, 29,44,53,38,63,85,89,82 }; double W = 165;
 		 * 
+		 * P07 is a set of 15 weights and profits for a knapsack of capacity 750, from
+		 * Kreher and Stinson, with an optimal profit of 1458.
 		 **/
 		int val[] = new int[] { 135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240 };
 		int wt[] = new int[] { 70, 73, 77, 80, 82, 87, 90, 94, 98, 106, 110, 113, 115, 118, 120 };
+		//wt: coefficiency of limitation <=
+		//val: profit, same as total day count
 		double W = 750;
 
-		int n = val.length;
-		double res = driver(W, wt, val, n);
-		System.out.println(res);
-		for (boolean e : getLastSelection(n, res, wt, val, W)) {
+		for (boolean e : handler(W, wt, val)) {
 			System.out.print((e) ? 1 : 0);
 		}
 
